e4a3a524fcc9fe71116bf20b55ad14116857b83f
Osabuohien Randalf Ehigiator <107191688+guyrandalf@users.noreply.github.com>
2023-04-27 06:24:58 +0100

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is your magic number? ");
+        // int user_magic_number = int.Parse(Console.ReadLine());
+        
+        int user_guess = -1;
+
+        // Stretch challenge
+        int attempts = 0;
+
+        Random randomGenerator = new Random();
+        int user_magic_number = randomGenerator.Next(1, 11);
+
+        while (user_magic_number != user_guess)
+        {
+            Console.Write("What is your guess? ");
+            user_guess = int.Parse(Console.ReadLine());
+            attempts += 1;            
+
+            if (user_guess == user_magic_number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.Write("You made a total attempt of " + attempts);
+            }
+            else if (user_guess < user_magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
     }
 }
